5e58ec2b51522098e1522efd51734fc0f6fbcb32
Vadzim Samal <casderiopus@gmail.com>
2024-06-01 00:34:47 +0300

    conf: update project config

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,7 +1,29 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  pageExtensions: ['page.tsx', 'api.ts'],
+  trailingSlash: false,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
+  output: 'standalone',
+  experimental: {
+    // this includes files from the monorepo base two directories up
+    outputFileTracingRoot: path.join(__dirname, '../../'),
+  },
 };
 
 export default nextConfig;
--- apps/web/src/pages/layout.tsx
+++ apps/web/src/pages/layout.tsx
@@ -1,7 +1,10 @@
-import '@mantine/core/styles.css';
 import React from 'react';
+
 import { MantineProvider, ColorSchemeScript } from '@mantine/core';
-import { theme } from '../theme';
+
+import '@mantine/core/styles.css';
+
+import { theme } from 'theme';
 
 export const metadata = {
   title: 'Mantine Next.js template',
@@ -12,14 +15,16 @@ const RootLayout = ({ children }: { children: any }) => (
   <html lang="en">
     <head>
       <ColorSchemeScript />
-      <link rel="shortcut icon" href="/favicon.svg" />
       <meta
         name="viewport"
         content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
       />
     </head>
+
     <body>
-      <MantineProvider theme={theme}>{children}</MantineProvider>
+      <MantineProvider theme={theme}>
+        {children}
+      </MantineProvider>
     </body>
   </html>
 );
