aa80802bb765a76d25e23339cba0f2959d1d21da
Vadzim Samal <casderiopus@gmail.com>
2024-06-07 19:07:28 +0300

    feat: add static serving

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,16 +1,21 @@
+import { join } from 'path';
+
 import { Logger, Module } from '@nestjs/common';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+
+import { AuthModule } from 'auth/auth.module';
+import { UsersModule } from 'users/users.module';
+import { GenresModule } from 'genres/genres.module';
+
+import { getDataBaseConfig } from './config/db.config';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 import { LoggerInterceptor } from './logger/logger.interceptor';
-import { getDataBaseConfig } from './config/db.config';
-import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
-import { GenresModule } from './genres/genres.module';
 
 @Module({
   imports: [
@@ -19,6 +24,9 @@ import { GenresModule } from './genres/genres.module';
       imports: [ConfigModule],
       useFactory: getDataBaseConfig,
       inject: [ConfigService],
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
     }),
     AuthModule,
     UsersModule,
